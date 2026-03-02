96365d29784e89b913db393275d8c49a63da5089
vorhitALONE <117316408+vorhitALONE@users.noreply.github.com>
2026-03-02 09:13:57 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,25 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "RandStuff"
+  title: "RandStuff - Генератор случайных чисел",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-          rel="stylesheet"
-        />
+        {/* Tailwind CDN как в исходном html */}
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lobster&family=PT+Sans:ital,wght@0,400;0,700;1,400&family=Times+New+Roman&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body>{children}</body>
